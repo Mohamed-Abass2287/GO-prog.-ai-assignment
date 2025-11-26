b2b3eff608eccdfa29125b400b65d3f177f8ab59
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-11-26 22:05:16 +0300

    corrected main

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main 
 
-import "fmt" // 2. Import: Brings in the "fmt" (format) package for printing text
-
-// 3. Main Function: The entry point where the program starts running
+import "fmt" 
 func main() {
-    // 4. Println prints the text to the console and adds a new line
+    
     fmt.Println("Hello, Go! I am ready to build.")
 }
