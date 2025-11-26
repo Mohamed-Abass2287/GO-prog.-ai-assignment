539c2f1c8c7c6a9279c846e584ec75b7fdb4a807
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-11-26 22:02:57 +0300

    1st commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main 
+
+import "fmt" // 2. Import: Brings in the "fmt" (format) package for printing text
+
+// 3. Main Function: The entry point where the program starts running
+func main() {
+    // 4. Println prints the text to the console and adds a new line
+    fmt.Println("Hello, Go! I am ready to build.")
+}
